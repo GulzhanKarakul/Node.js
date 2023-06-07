cfc0ffdce61a269e53a1f363c36f7821f5bdb633
Gulzhan <114820316+GulzhanKarakul@users.noreply.github.com>
2023-06-08 00:47:38 +0600

    hW
    nodeJs first homeWork

---
+++ myCowSay/index.js
@@ -0,0 +1,8 @@
+const Animal = require('./test/moduleCowsay.js');
+
+let animal = new Animal();
+
+animal.catSay('Hello, Vitaliy!');
+animal.monkeySay('Hello, Vitaliy!');
+animal.elephantSay('Hello, Vitaliy!');
+animal.duckSay('Hello, Vitaliy!');
