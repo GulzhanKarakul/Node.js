ac730c8b2cff30f46f034f3ab35d19cdbee41c01
Gulzhan <114820316+GulzhanKarakul@users.noreply.github.com>
2023-06-21 00:45:45 +0600

    up

---
+++ fileSystem/homeWork/thirdTask/convert.js
@@ -0,0 +1,79 @@
+// Виталий, я прикола ради сделала еще в константу чтобы в js файл еще записывался объект...
+
+// ГОТОВЫЕ ПРИМЕРЫ:
+// Во-первых: для конвертации json файла в xml и наоборот
+// node convert.js example2.json example1.xml
+// или node convert.js example.xml example.json
+
+// Во-вторых: для конвертации json или xml файла в js
+// node convert.js example.xml example3.js
+// node convert.js example2.json example3.js
+
+import fs from 'fs';
+import xml2js from 'xml2js';
+
+const log = console.log;
+const parser = new xml2js.Parser();
+const builder = new xml2js.Builder();
+const args = process.argv.slice(2);
+
+let sourceFNameArray = args[0].split('.');
+let destinFNameArray = args[1].split('.');
+
+if(sourceFNameArray[1] === 'json' && 
+   destinFNameArray[1] === 'xml')
+{
+    fs.readFile(args[0], 'utf8', (err, data) => {
+        if(err) throw err;
+        data = JSON.parse(data);
+        let result = builder.buildObject(data);
+        fs.writeFile(args[1],result, (err) => {
+            if(err) throw err;
+            log(`${args[0]} has been convert and written to ${args[1]}`);
+        });
+    });
+}
+else if(sourceFNameArray[1] === 'xml' &&
+        destinFNameArray[1] === 'json')
+{
+    fs.readFile(args[0], 'utf8', (err, data) => {
+        if(err) throw err;
+        parser.parseString(data, function (err, result) {
+            result = JSON.stringify(result, null, 2);
+            fs.writeFile(args[1],result, (err) => {
+                if(err) throw err;
+                log(`${args[0]} has been convert and written to ${args[1]}`);
+            });
+        });
+    });
+}
+else if(sourceFNameArray[1] === 'json' &&
+        destinFNameArray[1] === 'js')
+{
+    fs.readFile(args[0], 'utf8', (err, data) => {
+        if(err) throw err;
+        fs.writeFile(args[1], `const result = ${data}`, (err) => {
+            if(err) throw err;
+            log(`${args[0]} has been convert and written to ${args[1]}`);
+        });
+    });
+}
+else if(sourceFNameArray[1] === 'xml' && 
+        destinFNameArray[1] === 'js')
+{
+    fs.readFile(args[0], 'utf8', (err, data) => {
+        if(err) throw err;
+        log(data);
+        parser.parseString(data, function (err, result) {
+            result = JSON.stringify(result)
+            fs.writeFile(args[1], `const result = ${result}`, (err) => {
+                if(err) throw err;
+                log(result);
+                log(`${args[0]} has been convert and written to ${args[1]}`);
+            });
+        });
+    });
+}
+else {
+    log("You entered the wrong command.");
+}
